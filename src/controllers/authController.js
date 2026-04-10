f43825cbe9f57b36dc8148faffd4abf48a949164
blur <thetobiterra@gmail.com>
2026-04-11 00:32:04 +0100

    changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,16 @@
-const register = (req, res) => {
+import { prisma } from "../config/db.js";
+
+const register = async (req, res) => {
   const { name, email, password } = req.body;
-  res.json(body);
+  const userExists = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ error: "user already exists with this email" });
+  }
 };
 
 export { register };
